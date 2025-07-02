6e290c45f9e8ccc421a6f9ba2d8e2144395ca6c7
Bhavya-616 <152218611+Bhavya-616@users.noreply.github.com>
2025-07-02 16:01:46 +0530

    Add files via upload

---
+++ TableQ.sql
@@ -0,0 +1,40 @@
+-- create database EMclinic;
+use EMclinic;
+-- create table paitents(
+-- id int,
+-- p_name varchar(100),
+-- contact_no int,
+-- visting_time datetime,
+-- address varchar(255)
+-- );
+
+-- insert into paitents(id,p_name,contact_no,visting_time,address) values
+-- (1,'Armin',68545875,'2025-07-02 12:12:12','Paradise Island'),
+-- (1,'Levi',95478652,'2025-07-02 13:15:50','Paradise Island'),
+-- (1,'Sasha',65155147,'2025-07-02 18:20:01','Paradise Island')
+
+-- select * from paitents;
+-- select * from paitents where address = 'Paradise Island';
+-- select * from paitents where visting_time > '2025-07-01';
+-- select * from paitents where address != 'Paradise Island';
+-- select * from paitents; 
+-- select * from paitents where address= 'Paradise Island' and visting_time > '12:12:12';
+-- select * from paitents where address= 'Paradise Island' and visting_time > '12:12:13';
+-- select * from paitents where p_name like 'S%';
+-- select * from paitents where p_name like '%i';
+-- select * from paitents where p_name like '%i%';
+-- select * from paitents where p_name like '%a%';
+-- select * from paitents where p_name like '_ha';
+-- select * from paitents where p_name like '___ha';
+
+-- list of all the paitents in decending order of visting time
+-- select * from paitents order by visting_time;
+-- select * from paitents order by visting_time DESC;
+-- select * from paitents order by p_name DESC;
+-- select * from paitents order by p_name;
+-- select * from paitents limit 2;
+-- give me the name of the paitent that visited the clinic last
+-- select * from paitents;
+-- select * from paitents order by visting_time DESC limit 1;
+
+
