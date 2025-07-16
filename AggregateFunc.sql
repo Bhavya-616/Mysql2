8ebc667fc6804d7c2002b72dcab921338e565d13
Bhavya-616 <152218611+Bhavya-616@users.noreply.github.com>
2025-07-16 16:14:12 +0530

    Add files via upload

---
+++ AggregateFunc.sql
@@ -0,0 +1,24 @@
+-- create database D2Dc;
+use D2Dc;
+-- create table meteor_shower(id int, shower_name text, observation_date date, duration_min int, meteors_seen int, location text);
+-- insert into meteor_shower (id,shower_name,observation_date,duration_min,meteors_seen,location) values
+-- (1,'Perseids','2024-08-12',120,50,'Arizona'),
+-- (2,'Geminids','2024-12-14',90,65,'Nevada'),
+-- (3,'Leonids','2024-11-17',60,30,'Arizona'),
+-- (4,'Quadrantids','2024-01-03',80,40,'Colorado'),
+-- (5,'Eta Aquariids','2024-05-06',100,55,'Nevada');
+-- select * from meteor_shower;
+
+-- select sum(meteors_seen) as total from meteor_shower;
+-- select avg(meteors_seen) as avg from meteor_shower;
+-- select max(duration_min) from meteor_shower;
+-- select min(meteors_seen) from meteor_shower;
+-- select count(observation_date) from meteor_shower;
+-- select sum(meteors_seen) from meteor_shower where location = 'Nevada';
+-- select avg(duration_min) from meteor_shower where location = 'Arizona';
+-- select count(id) from meteor_shower where meteors_seen>40;
+-- select max(meteors_seen) from meteor_shower where location != 'Arizona';
+-- select sum(duration_min) from meteor_shower where year(observation_date)= '2024';
+
+
+
